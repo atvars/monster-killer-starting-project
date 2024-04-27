0714e4b4ea689feeb838f57a7a1247af013f3ffe
atvars <avelavs@gmail.com>
2024-04-27 22:35:09 +0000

    added strong_attack and combined in function

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,4 +1,5 @@
 const ATTACK_VALUE = 10;
+const STRONG_ATTACK_VALUE = 17;
 const MONSTER_ATTACK_VALUE = 14;
 
 let choosenMaxLife = 100;
@@ -7,19 +8,33 @@ let currentPlayerHealth = choosenMaxLife;
 
 adjustHealthBars(choosenMaxLife);
 
-function attackHandler() {
-    const damage = dealMonsterDamage(ATTACK_VALUE);
-    currentMonsterHealth -= damage;
-    const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE)
-    currentPlayerHealth -= playerDamage
-    if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
-        alert('You Won!')
-    } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
-        alert('You Lost!')
-    } else if (currentMonsterHealth <= 0 && currentPlayerHealth <= 0 ) {
-        alert('Its a Draw!')
-    }
-
+function attackMonster(mode) {
+  let maxDamage;
+  if (mode === 'ATTACK') {
+    maxDamage = ATTACK_VALUE;
+  } else if (mode === 'STRONG_ATTACK') {
+    maxDamage = STRONG_ATTACK_VALUE;
+  }
+  const damage = dealMonsterDamage(maxDamage);
+  currentMonsterHealth -= damage;
+  const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
+  currentPlayerHealth -= playerDamage;
+  if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
+    alert('You Won!');
+  } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
+    alert('You Lost!');
+  } else if (currentMonsterHealth <= 0 && currentPlayerHealth <= 0) {
+    alert('Its a Draw!');
+  }
 }
 
+function attackHandler() {
+    attackMonster('ATTACK');
+}
+function strongAttackHandler() {
+    attackMonster('STRONG_ATTACK');
+}
+
+// event handlers added to the buttons
 attackBtn.addEventListener('click', attackHandler);
+strongAttackBtn.addEventListener('click', strongAttackHandler);
